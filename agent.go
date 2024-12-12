52d9cb5d7bddb595e1abedd6b99b1c659fb23cad
Bryan Kneis <bryan.kneis@loft.sh>
2024-12-12 15:46:25 +0000

    Allow dir to be specified

--- agent.go
+++ agent.go
@@ -33,12 +33,16 @@ func AgentRequested(sess Session) bool {
 	return sess.Context().Value(contextKeyAgentRequest) == true
 }
 
-// NewAgentListener sets up a temporary Unix socket that can be communicated
-// to the session environment and used for forwarding connections.
-func NewAgentListener() (net.Listener, string, error) {
-	dir, err := os.MkdirTemp("", agentTempDir)
-	if err != nil {
-		return nil, "", err
+// NewAgentListener sets up a temporary Unix socket, if dir is not specified, that can be communicated
+// to the session environment and used for forwarding connections. If dir is specified, it will use the directory
+// to create the socket file.
+func NewAgentListener(dir string) (net.Listener, string, error) {
+	var err error
+	if dir == "" {
+		dir, err = os.MkdirTemp("", agentTempDir)
+		if err != nil {
+			return nil, "", err
+		}
 	}
 	l, err := net.Listen("unix", filepath.Join(dir, agentListenFile))
 	if err != nil {
